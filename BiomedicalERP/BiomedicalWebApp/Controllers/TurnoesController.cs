491f41fa0b0ea2ffaed4c02af4b31eaf539408dd
JohnnyJoestar324 <martinezjohnny324@gmail.com>
2022-10-12 23:21:10 -0600

    Se agregan botones de Turno y Medico a la vista de Pacientes

---
+++ BiomedicalERP/BiomedicalWebApp/Controllers/TurnoesController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BiomedicalWebApp.Models;
+
+namespace BiomedicalWebApp.Controllers
+{
+    public class TurnoesController : Controller
+    {
+        private readonly BiomedicalCenterContext _context;
+
+        public TurnoesController(BiomedicalCenterContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Turnoes
+        public async Task<IActionResult> Index()
+        {
+            var biomedicalCenterContext = _context.Turnos.Include(t => t.EstadoNavigation);
+            return View(await biomedicalCenterContext.ToListAsync());
+        }
+
+        // GET: Turnoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Turnos == null)
+            {
+                return NotFound();
+            }
+
+            var turno = await _context.Turnos
+                .Include(t => t.EstadoNavigation)
+                .FirstOrDefaultAsync(m => m.IdTurno == id);
+            if (turno == null)
+            {
+                return NotFound();
+            }
+
+            return View(turno);
+        }
+
+        // GET: Turnoes/Create
+        public IActionResult Create()
+        {
+            ViewData["Estado"] = new SelectList(_context.TurnoEstados, "IdEstado", "IdEstado");
+            return View();
+        }
+
+        // POST: Turnoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdTurno,FechaTurno,Estado,Observacion")] Turno turno)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(turno);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Estado"] = new SelectList(_context.TurnoEstados, "IdEstado", "IdEstado", turno.Estado);
+            return View(turno);
+        }
+
+        // GET: Turnoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Turnos == null)
+            {
+                return NotFound();
+            }
+
+            var turno = await _context.Turnos.FindAsync(id);
+            if (turno == null)
+            {
+                return NotFound();
+            }
+            ViewData["Estado"] = new SelectList(_context.TurnoEstados, "IdEstado", "IdEstado", turno.Estado);
+            return View(turno);
+        }
+
+        // POST: Turnoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdTurno,FechaTurno,Estado,Observacion")] Turno turno)
+        {
+            if (id != turno.IdTurno)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(turno);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TurnoExists(turno.IdTurno))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["Estado"] = new SelectList(_context.TurnoEstados, "IdEstado", "IdEstado", turno.Estado);
+            return View(turno);
+        }
+
+        // GET: Turnoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Turnos == null)
+            {
+                return NotFound();
+            }
+
+            var turno = await _context.Turnos
+                .Include(t => t.EstadoNavigation)
+                .FirstOrDefaultAsync(m => m.IdTurno == id);
+            if (turno == null)
+            {
+                return NotFound();
+            }
+
+            return View(turno);
+        }
+
+        // POST: Turnoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Turnos == null)
+            {
+                return Problem("Entity set 'BiomedicalCenterContext.Turnos'  is null.");
+            }
+            var turno = await _context.Turnos.FindAsync(id);
+            if (turno != null)
+            {
+                _context.Turnos.Remove(turno);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TurnoExists(int id)
+        {
+          return _context.Turnos.Any(e => e.IdTurno == id);
+        }
+    }
+}
